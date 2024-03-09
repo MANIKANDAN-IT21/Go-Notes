2252c5db2883537590dabd734590f76c372d2947
MANIKANDAN-IT21 <manikandan.it21@bitsathy.ac.in>
2024-03-09 21:43:24 +0530

    Variblle

--- First.go
+++ First.go
@@ -1,4 +1,5 @@
 package main
+
 import "fmt"
  func main(){
 	fmt.Printf("manikandan")
---
+++ Variable.go
@@ -0,0 +1,27 @@
+// Datatypes
+
+// number=123,Float32,Float64
+// String="manikandan"
+// Boolean=True or False
+// Derived Datatypes=(array,pointer,union,struture)
+// ----------------------------------***------------------------------------------------------
+// ----------------------------var variablename type =value--------------
+// var number int=45
+// var number int
+// number=45
+// --String
+// var name string="mani"
+// var name string 
+// name ="mani"
+// var name="mani"
+// --------------------------------variablename:=value-----------------------------------------
+// number:=78
+// name:="manikanda"
+package main
+import "fmt"
+func main(){
+	 num:=10.2
+	 mani:=true
+	fmt.Printf("%f", num)
+	fmt.Printf(mani)
+}
